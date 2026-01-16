7bd7ec23b0b0a0a504a17e339b59dc072dc3b9fd
David Loukidelis <david@loukidelis.com>
2026-01-16 12:18:28 -0500

    add gitsigns shortcut

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,3 +14,7 @@ vim.keymap.set("n", "<leader>cp", function()
   vim.fn.setreg("+", relative_path)
   print("Copied: " .. relative_path)
 end, { desc = "Copy relative path to clipboard" })
+
+vim.keymap.set("n", "<leader>gb", function()
+  require("gitsigns").blame()
+end, { desc = "Git blame" })
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,15 +1,29 @@
 return {
-  {
-    "lewis6991/gitsigns.nvim",
-
-    lazy = false,
-    event = "VeryLazy",
-    keys = {
-      {
-        "<leader>bg",
-        "<cmd>Gitsigns blame<cr>",
-        desc = "Toggle Git Blame Gutter",
-      },
+  "lewis6991/gitsigns.nvim",
+  enabled = true,
+  event = "VeryLazy",
+  opts = {
+    current_line_blame = true, -- Toggle with :Gitsigns toggle_current_line_blame
+    current_line_blame_opts = {
+      delay = 300,
     },
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
+
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      -- Navigation
+      map("n", "]c", gs.next_hunk)
+      map("n", "[c", gs.prev_hunk)
+
+      -- Actions
+      map("n", "<leader>hs", gs.stage_hunk)
+      map("n", "<leader>hr", gs.reset_hunk)
+      map("n", "<leader>hb", gs.blame_line)
+    end,
   },
 }
